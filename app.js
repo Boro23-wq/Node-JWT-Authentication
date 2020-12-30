f95cec86d806c425821a489d13b5432d19585011
Boro23 <404slimboysavage@gmail.com>
2020-12-30 22:38:51 +0530

    initial-commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI =
+  'mongodb+srv://Boro:galIEe04R2JG3pwn@cluster0.eutdg.gcp.mongodb.net/Node-JWT-Auth?retryWrites=true&w=majority';
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(3000))
+  .then(console.log('MongoDB Connected!'))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
